977a9868cd91168bdafcb1d927ee1ddacb1e6178
omochimogumogu666 <h01itaku@gmail.com>
2023-10-20 06:05:12 +0900

    Renderデプロイのための環境設定を追記2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Rails.application.configure do
+    config.hosts "hello-app-0b5t.onrender.com"
+  end
 end
